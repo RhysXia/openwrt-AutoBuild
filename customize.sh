2dd46a4f2737edb08c116cd1a42ecfb713d3e8e1
Rhys Xia <xrs4433@outlook.com>
2019-12-17 23:35:06 +0800

    update

--- customize.sh
+++ customize.sh
@@ -7,4 +7,4 @@ sed -i 's/192.168.1.1/10.20.10.50/g' package/base-files/files/bin/config_generat
 cd package
 mkdir openwrt-packages
 cd openwrt-packages
-git clone https://github.com/Ameykyl/luci-app-koolproxyR
+# git clone --depth 1 https://github.com/Ameykyl/luci-app-koolproxyR
