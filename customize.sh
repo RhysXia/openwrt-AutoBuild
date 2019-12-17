1ccb26ceae62f029c50f5f48f78fc48cad84e2e9
Rhys Xia <xrs4433@outlook.com>
2019-12-17 22:12:00 +0800

    update

--- customize.sh
+++ customize.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
+# 修改默认ip
+sed -i 's/192.168.1.1/10.20.10.50/g' package/base-files/files/bin/config_generate
+
 # 添加额外的插件和主题
 cd package
 mkdir openwrt-packages
 cd openwrt-packages
 git clone https://github.com/Ameykyl/luci-app-koolproxyR
-git clone https://github.com/rosywrt/luci-theme-rosy
-
-# 修改默认ip
-# sed -i 's/192.168.1.1/10.20.10.50/g' package/base-files/files/bin/config_generate
