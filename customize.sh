a00baad80af17c23b969e8ffced5ef64350b4c45
Rhys Xia <xrs4433@outlook.com>
2020-04-29 21:34:39 +0800

    update

--- customize.sh
+++ customize.sh
@@ -2,7 +2,7 @@
 
 # 修改默认ip
 sed -i 's/192.168.1.1/10.10.20.20/g' package/base-files/files/bin/config_generate
-sed -i 's/set network.$1.netmask=\'$netm\'set network.$1.netmask=\'$netm\'\n\tset network.$1.gateway=\'10.10.20.1\'\n\tset network.$1.dns=\'10.10.20.10\'/g' package/base-files/files/bin/config_generate
+sed -i 's/set network.$1.netmask=\'$netm\'/set network.$1.netmask=\'$netm\'\n\tset network.$1.gateway=\'10.10.20.1\'\n\tset network.$1.dns=\'10.10.20.10\'/g' package/base-files/files/bin/config_generate
 
 cat package/base-files/files/bin/config_generate
 
